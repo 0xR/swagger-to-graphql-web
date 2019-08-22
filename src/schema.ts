5366fae69dd451842d08de82ae4cb3dfaf0f3e42
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-08-22 23:04:06 +0200

    Replace request-promise with fetch

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,7 @@
-import requestPromise from "request-promise";
 import {
   createSchema as createSchemaFromSwagger,
   CallBackendArguments
-} from "swagger-to-graphql";
+} from 'swagger-to-graphql';
 
 // @ts-ignore
 export const createSchema = (url: string) =>
@@ -11,19 +10,35 @@ export const createSchema = (url: string) =>
     async callBackend({
       requestOptions: { method, body, baseUrl, path, query, headers, bodyType }
     }: CallBackendArguments<{}>) {
-      return requestPromise({
-        ...(bodyType === "json" && {
-          json: true,
-          body
-        }),
-        ...(bodyType === "formData" && {
-          form: body
-        }),
-        qs: query,
-        method,
-        headers,
-        baseUrl: `https://cors-anywhere.herokuapp.com/${baseUrl}`,
-        uri: path
-      });
+      const hasBody = Object.keys(body).length > 0;
+      const response = await fetch(
+        `https://cors-anywhere.herokuapp.com/${baseUrl}${path}?${new URLSearchParams(
+          query
+        )}`,
+        {
+          method,
+          ...(bodyType === 'json' && {
+            headers: {
+              'Content-Type': 'application/json',
+              ...headers
+            },
+            body: hasBody ? JSON.stringify(body) : undefined
+          }),
+          ...(bodyType === 'formData' && {
+            headers: headers,
+            body: hasBody ? new URLSearchParams(body) : undefined
+          })
+        }
+      );
+
+      const text = await response.text();
+      if (200 <= response.status && response.status < 300) {
+        try {
+          return JSON.parse(text);
+        } catch (e) {
+          return text;
+        }
+      }
+      throw new Error(`Response: ${response.status} - ${text}`);
     }
   });
