bb87cbd7764db3ab6118ec6fb02b85809b138c55
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-10-06 14:05:39 +0200

    Simplify fetch code

--- src/schema.ts
+++ src/schema.ts
@@ -4,32 +4,44 @@ import {
 } from 'swagger-to-graphql';
 import { safeLoad } from 'js-yaml';
 
+function getBodyAndHeaders(
+  body: any,
+  bodyType: 'json' | 'formData',
+  headers: { [key: string]: string } | undefined,
+) {
+  if (!body) {
+    return { headers };
+  }
+
+  if (bodyType === 'json') {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    };
+  }
+
+  return {
+    headers,
+    body: new URLSearchParams(body),
+  };
+}
+
 async function callBackend({
   requestOptions: { method, body, baseUrl, path, query, headers, bodyType },
 }: CallBackendArguments<{}>) {
-  const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/${baseUrl}${path}?${new URLSearchParams(
-      // Typedefs seem wrong
-      query as Record<string, string>,
-    )}`,
-    {
-      method,
-      ...(bodyType === 'json' && {
-        headers: {
-          'Content-Type': 'application/json',
-          ...headers,
-        },
-        body: body ? JSON.stringify(body) : undefined,
-      }),
-      ...(bodyType === 'formData' && {
-        headers: headers,
-        body: body ? new URLSearchParams(body) : undefined,
-      }),
-    },
-  );
+  const searchPath = query ? `?${new URLSearchParams(query as Record<string,string>)}` : '';
+  const url = `https://cors-anywhere.herokuapp.com/${baseUrl}${path}${searchPath}`;
+  const bodyAndHeaders = getBodyAndHeaders(body, bodyType, headers);
+  const response = await fetch(url, {
+    method,
+    ...bodyAndHeaders,
+  });
 
   const text = await response.text();
-  if (200 <= response.status && response.status < 300) {
+  if (response.ok) {
     try {
       return JSON.parse(text);
     } catch (e) {
@@ -38,6 +50,7 @@ async function callBackend({
   }
   throw new Error(`Response: ${response.status} - ${text}`);
 }
+
 export const createSchema = async (url: string) => {
   const swaggerSchema = await callBackend({
     context: {},
@@ -49,7 +62,8 @@ export const createSchema = async (url: string) => {
     },
   });
 
-  const parsedSchema = typeof swaggerSchema === 'string' ? safeLoad(swaggerSchema) : swaggerSchema;
+  const parsedSchema =
+    typeof swaggerSchema === 'string' ? safeLoad(swaggerSchema) : swaggerSchema;
 
   return createSchemaFromSwagger({
     swaggerSchema: parsedSchema,
